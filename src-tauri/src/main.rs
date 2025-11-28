5cb381fb20764019e7ed28d88a5eedd5379d48fc
Zevobla <ghost-brake-your-home@protonmail.com>
2025-11-28 14:43:46 +0300

    feat(tauri): add application menu and About metadata
    Replace the previous run call with a Tauri Builder that installs an
    application menu and About metadata. Adds File menu and wires the menu
    into the app.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,47 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::menu::{AboutMetadata, MenuBuilder, SubmenuBuilder};
+
 fn main() {
-    logisim_doma_lib::run()
+    tauri::Builder::default()
+        .setup(|app| {
+            let metadata = AboutMetadata {
+                name: Some("LogisimDoma".into()),
+                version: Some("0.1.0".into()),
+                copyright: Some(
+                    "Copyright © 2025 Artemii Matiazh.\nLicensed under the GNU AGPL v3.".into(),
+                ),
+                license: Some("Licensed under the GNU AGPL v3.".into()),
+                authors: Some(vec!["Zevobla".into()]),
+                website: Some("https://github.com/zevobla/logisim-doma".into()),
+                website_label: Some("LogisimDoma Source Code".into()),
+                comments: Some("A modern port of Logisim".into()),
+                ..Default::default()
+            };
+            let app_menu = SubmenuBuilder::new(app, "LogisimDoma")
+                .about_with_text("About LogisimDoma", Some(metadata))
+                .separator()
+                .services()
+                .separator()
+                .hide()
+                .hide_others()
+                .show_all()
+                .separator()
+                .close_window()
+                .build()?;
+            let file_menu = SubmenuBuilder::new(app, "File")
+                .text("open", "Open...")
+                .text("quit", "Quit")
+                .build()?;
+
+            let menu = MenuBuilder::new(app)
+                .items(&[&app_menu, &file_menu])
+                .build()?;
+            app.set_menu(menu)?;
+
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
